fd330c5b68749bd4da865e4479a566bbb420b576
오종화 <ojonghwa@naver.com>
2021-03-13 09:37:23 +0900

    프로필 페이지 추가, 이미지 업로드

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flaskext.markdown import Markdown      #p232
+from flaskext.markdown import Markdown  # p232
 from sqlalchemy import MetaData
 
 import config
 
-naming_convention = {       #p159
+naming_convention = {  # p159
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
@@ -17,13 +17,25 @@ db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
-def page_not_found(e):      #p323, 오류404 처리페이지
+def page_not_found(e):  # p323, 오류404 처리페이지
     return render_template('404.html'), 404
+
+
+def Bad_Request(e):
+    return render_template('400.html'), 400
+
+
+def Request_Entity_Too_Large(e):
+    return render_template('413.html'), 413
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+
+    app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+    # 다른 파일에서 app.config['UPLOAD_EXTENSIONS'] 인식하는 방법 필요
+    app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 
     # ORM, p159
     db.init_app(app)
@@ -33,15 +45,16 @@ def create_app():
         migrate.init_app(app, db)
     from . import models
 
-    # 블루프린트 
-    from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views   #p76,82,145,202,216
+    # 블루프린트
+    # p76,82,145,202,216
+    from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
 
-    app.register_blueprint(main_views.bp) 
-    app.register_blueprint(question_views.bp) 
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
     app.register_blueprint(comment_views.bp)
-    app.register_blueprint(vote_views.bp)       #p216
+    app.register_blueprint(vote_views.bp)  # p216
 
     # 필터, p131
     from .filter import format_datetime
@@ -52,5 +65,7 @@ def create_app():
 
     # 오류페이지, p323
     app.register_error_handler(404, page_not_found)
+    app.register_error_handler(400, Bad_Request)
+    app.register_error_handler(413, Request_Entity_Too_Large)
 
     return app
--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,21 +1,30 @@
-  
-import functools        #p174
-from flask import Blueprint, url_for, render_template, flash, request, session, g  #p150
-from werkzeug.security import generate_password_hash, check_password_hash   #p150
-from werkzeug.utils import redirect
+import functools
+import imghdr
+import os
+import random     # p174
+from flask import Blueprint, url_for, render_template, flash, abort
+from flask import request, session, g, current_app  # p150
+from werkzeug.security import generate_password_hash, check_password_hash  # p150
+from werkzeug.utils import redirect, secure_filename
+
+# Flask context globals
+# current_app : Application context - The application instance for the active application.
+# g : Application context - An object that the application can use for temporary storage during the handling of a request. This variable is reset with each request.
+# request : Request context - The request object, which encapsulates the contents of a HTTP request sent by the client.
+# session : Request context - The user session, a dictionary that the application can use to store values that are remembered between requests
 
 from .. import db
-from ..forms import UserCreateForm, UserLoginForm  #p142,150
-from ..models import User           #p140
+from ..forms import UserCreateForm, UserLoginForm  # p142,150
+from ..models import User  # p140
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-#p143, 회원가입 화면뷰 
-@bp.route('/signup/', methods=('GET', 'POST'))      #URL /signup/ 과 연결된 함수 
+# p143, 회원가입 화면뷰
+@bp.route('/signup/', methods=('GET', 'POST'))  # URL /signup/ 과 연결된 함수
 def signup():
-    form = UserCreateForm()     #p142
-    #POST방식 요청에는 계정등록을, GET방식 요청에는 계정등록을 하는 템플릿을 렌더링  
+    form = UserCreateForm()  # p142
+    # POST방식 요청에는 계정등록을, GET방식 요청에는 계정등록을 하는 템플릿을 렌더링
     if request.method == 'POST' and form.validate_on_submit():
         user = User.query.filter_by(userid=form.userid.data).first()
         if not user:
@@ -31,7 +40,7 @@ def signup():
     return render_template('auth/signup.html', form=form)
 
 
-#p151, 로그인 라우트 
+# p151, 로그인 라우트
 @bp.route('/login/', methods=('GET', 'POST'))
 def login():
     form = UserLoginForm()
@@ -51,24 +60,24 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-#p155, 모든 라우트 함수보다 먼저 load_logged_in_user() 함수가 실행되도록 함
-@bp.before_app_request       
+# p155, 모든 라우트 함수보다 먼저 load_logged_in_user() 함수가 실행되도록 함
+@bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
     if user_id is None:
         g.user = None
     else:
-        g.user = User.query.get(user_id)    #플라스크 컨택스트 변수, 전체 글로벌 변수
+        g.user = User.query.get(user_id)  # 플라스크 컨택스트 변수, 전체 글로벌 변수
 
 
-#p157, 로그아웃 라우트 
+# p157, 로그아웃 라우트
 @bp.route('/logout/')
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
 
 
-#p174, 로그인 상태관리 데코레이터 함수 
+# p174, 로그인 상태관리 데코레이터 함수
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
@@ -76,4 +85,42 @@ def login_required(view):
             return redirect(url_for('auth.login'))
         return view(**kwargs)
     return wrapped_view
-    
+
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+
+# ojonghwa, 2021.3.12
+# 로그인 사용자 프로필 소개 페이지 및 사진 업로드
+@bp.route('/profile/', methods=('GET', 'POST'))
+def profile():
+    if g.user is None:
+        return redirect(url_for('auth.login'))
+    else:
+        if request.method == 'POST':
+            uploaded_file = request.files['file']
+            #filename = uploaded_file.filename
+            filename = secure_filename(
+                uploaded_file.filename)  # 파일 디렉터리 변경 저장 차단
+
+            if filename != '':
+                file_ext = os.path.splitext(filename)[1]  # DB User 저장
+
+                # 파일용량 검사, 파일 확장자 조사, -> 파일 헤더 조사 필요
+                # if file_ext not in ['.jpg', '.png', 'gif'] or file_ext != validate_image(uploaded_file.stream):
+                if file_ext not in current_app.config['UPLOAD_EXTENSIONS']:
+                    abort(400)  # 400 오류발생, from flask import abort
+
+                #uploaded_file.save('pybo/static/uploads/' + uploaded_file.filename)
+                uploaded_file.save('pybo/static/uploads/' +
+                                   g.user.userid + '.jpg')
+
+        file = g.user.userid + '.jpg'
+        # 캐시방지
+        return render_template('auth/profile.html', image_file=file, ver=random.random())
