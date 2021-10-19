e268e9d60db74a50ad6f9beb5d7bfcac19dc72b5
ojonghwa <ojonghwa@naver.com>
2021-10-19 19:23:27 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
 
 #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-#    user='dbmasteruser', pw='=eKmx$&ymn$wNC|rNT$SX55*RdjKK1G&',
-#    url='ls-be78fd2c2e6b5261442d7480def69d46b156e2c9.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
+#    user='dbmasteruser', pw='abcd',
+#    url='ap-northeast-2.rds.amazonaws.com',
 #    db='flask_pybo')
